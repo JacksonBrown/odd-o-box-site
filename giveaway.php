3d52f77ea2a9f9382ae73e6ce30dd2901ae9e762
JacksonBrown <jacksonconnerbrown@gmail.com>
2016-03-27 13:19:51 -0400

    We're all dead here.

---
+++ giveaway.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>I am SavedIn1080p</title>
+
+	<!-- Load fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+
+	<!-- Load css styles -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	
+	<!-- Load jQuery -->
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+	<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="js/unslider.js"></script>
+	
+	<!-- Set Title Logo -->
+	<link rel="shortcut icon" href="css/img/diamond.png" />
+
+	<!-- CSS Additions -->
+	<style type="text/css">
+		html {
+			overflow: -moz-scrollbars-none; 
+			 height: 100%;
+		}
+
+		.container {
+  			display: block;
+  			-webkit-box-sizing: border-box;
+ 			-moz-box-sizing: border-box;
+  			box-sizing: border-box;
+  			text-shadow: 0 0 5px #666;
+  			-webkit-animation: glow-anim 12s infinite;
+  			-moz-animation: glow-anim 12s infinite;
+  			animation: glow-anim 12s infinite;
+		}
+
+		.container-two {
+			font-family: 'Roboto Slab', serif;
+		}
+
+		@-webkit-keyframes glow-anim {
+  			25% {
+    				text-shadow: 0 0 25px #FF3E34;
+    				color: #FF3E34;
+  			}
+  			50% {
+    				text-shadow: 0 0 25px #60FF67;
+    				color: #60FF67;
+  			}
+  			75% {
+    				text-shadow: 0 0 25px #4C79FF;
+    				color: #4C79FF;
+  			}
+		}
+		@-moz-keyframes glow-anim {
+  			25% {
+    				text-shadow: 0 0 25px #FF3E34;
+   	 			color: #FF3E34;
+  			}
+  			50% {
+    				text-shadow: 0 0 25px #60FF67;
+    				color: #60FF67;
+  			}
+  			75% {
+    				text-shadow: 0 0 25px #4C79FF;
+    				color: #4C79FF;
+  			}
+		}
+		@keyframes glow-anim {
+  			25% {
+    				text-shadow: 0 0 25px #FF3E34;
+    				color: #FF3E34;
+  			}
+  			50% {
+    				text-shadow: 0 0 25px #60FF67;
+    				color: #60FF67;
+  			}
+  			75% {
+    				text-shadow: 0 0 25px #4C79FF;
+   	 			color: #4C79FF;
+  			}
+		}
+
+		.btn-two{
+			background: none;
+			border: 3px solid #fff;
+			font-family: 'Montserrat', sans-serif;
+			color: #fff;
+		}
+
+		.btn-two:hover{
+			background: #fff;
+			color: #000;
+			transition: 0.6s;
+		}
+
+		.btn-two:visited{
+			
+		}
+
+		.btn-three{
+			background: none;
+			border: 3px solid #000;
+			color: #000;
+			font-family: 'Montserrat', sans-serif;
+		}
+
+		.btn-three:hover{
+			background: #000;
+			color: #fff;
+			transition: 0.6s;
+			border-color: #000;
+		}
+
+		.upper-bg{
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+		}
+
+
+		::webkit-scrollbar{
+			display: none;
+		}
+
+	</style>
+
+	<!-- Custom JS -->
+	<script type="text/javascript">
+		$(function() {
+
+			$("#slideshow > div:gt(0)").hide();
+
+			setInterval(function() {
+				$('#slideshow > div:first')
+			    		.fadeOut(1000)
+			    		.next()
+			    		.fadeIn(1000)
+			    		.end()
+			    		.appendTo('#slideshow');
+				},  5000);
+
+		});
+
+		var wss_i = 0;
+		var wss_array = ["\"You Smart\"","\"You Loyal\"" ,"\"We Da Best\"","\"Anotha One\""];
+		var wss_elem;
+		function wssNext(){
+			wss_i++;
+			wss_elem.style.opacity = 0;
+			if(wss_i > (wss_array.length - 1)){
+				wss_i = 0;
+			}
+			setTimeout('wssSlide()', 2000);
+		}
+
+		function wssSlide(){
+			wss_elem.innerHTML = wss_array[wss_i];
+			wss_elem.style.opacity = 1;
+			setTimeout('wssNext()', 4000);
+		}
+	</script>
+
+</head>
+<body style="background: #333;">
+
+
+	<!-- BEGIN UPPER BG -->
+	<div class="upper-bg home home-fullscreen home" id="home" style="background: #000;">
+		<!-- BEGIN UPPER BG LANDING HEADER -->
+		<div class="container" style="overflow: hidden;">
+			<div class="header-info" style="">
+				<h1 class="header-info" style="margin-top: 10%; color: #fff;">Show The Participants!</h1>
+				<a data-scroll class="btn btn-two" href="#partic" >Reveal</a>
+			</div>
+		</div>
+		<!-- END UPPER BG LANDING HEADER -->
+	</div>
+	<!-- END UPPER BG -->
+
+	<!-- BEGIN UPPER BG -->
+	<div class="upper-bg home home-fullscreen partic" id="partic" style="background: #000;">
+		<div class="container">
+			<div class="header-info" style="">
+				<h1 class="header-info" style="margin-top: 10%; color: #fff;">List of people here.</h1>
+				<a data-scroll class="btn btn-two" href="#winner" >Show The Winner</a>
+			</div>
+		</div>
+
+		<div class="container-two">
+			
+		</div>
+	</div>
+
+	<!-- BEGIN UPPER BG -->
+	<div class="upper-bg home home-fullscreen winner" id="winner" style="background: #fff;">
+		<div class="container">
+			<div class="header-info" style="">
+				<h1 class="header-info" style="margin-top: 10%; color: #000;">USERNAME!</h1>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Load Booststrap -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/smooth-scroll.js"></script>
+
+	<script src="js/app.js"></script>
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>I am SavedIn1080p</title>
+
+	<!-- Load fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Raleway:900' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=VT323' rel='stylesheet' type='text/css'>
+
+	<!-- Load css styles -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="unslider/dist/css/unslider.css" />
+	
+	<!-- Load jQuery -->
+	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+	<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="//code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/unslider.min.js"></script>
+	<script src="js/unslider.js"></script>
+	
+	<!-- Load Unslider -->
+	<script src="//unslider.com/unslider.min.js"></script>
+	
+	<!-- Set Title Logo -->
+	<link rel="shortcut icon" href="css/img/diamond.png" />
+
+	<!-- Set Custom JS -->
+	<script type="text/javascript">
+		var wss_i = 0;
+		var wss_array = ["\"You Smart\"","\"You Loyal\"" ,"\"We Da Best\"","\"Anotha One\""];
+		var wss_elem;
+		function wssNext(){
+			wss_i++;
+			wss_elem.style.opacity = 0;
+			if(wss_i > (wss_array.length - 1)){
+				wss_i = 0;
+			}
+			setTimeout('wssSlide()', 2000);
+		}
+		function wssSlide(){
+			wss_elem.innerHTML = wss_array[wss_i];
+			wss_elem.style.opacity = 1;
+			setTimeout('wssNext()', 4000);
+		}
+	</script>
+
+	<!-- Set Custom CSS -->
+	<style type="text/css">
+		#wss{
+			opacity:0;
+			-webkit-transition:opacity 1.0s linear 0s;
+			transition:opacity 1.0s linear 0s;
+		}
+	</style>
+
+</head>
+<body>
+
+
+
+<!-- FACEBOOK JS SDK -->
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- END FACEBOOK JS SDK -->
+
+
+
+	
+	<!-- BEGIN UPPER BG -->
+	<div class="upper-bg home home-fullscreen paral" id="home">
+
+
+		<!-- BEGIN LOGO -->
+		<div class="logo col-md-6">
+			<nav class="navigate">
+			<h1 class="logo-header" style="font-size: 25px; font-family: 'VT323', sans-serif;">SavedIn1080p</h1>
+			</nav>
+		</div>
+		<!-- END LOGO -->
+
+
+		<!-- BEGIN NAVIGATION -->
+		<div class="col-md-6">
+			<nav class="nav navigate">
+				<ul>
+
+					<div class="col-md-3">
+					<a href="https://www.youtube.com/user/00IAmSaved00" target="_blank">Channel</a>
+					</div>
+
+					<div class="col-md-3">
+					<a data-scroll href="#giveaway">Media</a>
+					</div>
+
+					<div class="col-md-3">
+					<a href="http://savedin1080p.com/mybb" target="_blank">Forums</a>
+					</div>
+
+				</ul>
+			</nav>
+		</div>	
+		<!-- END NAVIGATION -->
+
+
+		<!-- BEGIN UPPER BG LANDING HEADER -->
+		<div class="container">
+			<div class="header-info">
+					<h1 class="landing-header" style="color: #fff;"><span id="wss"></span></h1>
+					<script>wss_elem = document.getElementById("wss"); wssSlide();</script>
+					<a data-scroll href="#second-half" class="btn btn-primary" target="_blank">Newest Video</a>
+			</div>
+		</div>
+		<!-- END UPPER BG LANDING HEADER -->
+
+	</div>
+	<!-- END UPPER BG -->
+	
+	<!-- BEGIN SECOND HALF -->
+	
+	<section id="second-half" class="upper-bg home home-fullscreen second-half">
+
+		<div class="mask"></div>
+
+		<div class="container">
+			<div class="row">
+				<div class="column">
+					<div class="second-half-item">
+						<div class="video-view">
+							<iframe src="https://www.youtube.com/embed/zOT2Vps1a1Y?list=PLMGscQtuGnF4HV-pAk4351nhG4PY5gq_n" frameborder="0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+	<!-- END SECOND HALF-->
+
+	<!-- BEGIN GIVEAWAY -->
+	<section id="giveaway" class="giveaway">
+					
+		<!-- CREATE GIVEAWAY HEADER --> 
+		<!--
+		<div class="header-wrapper">
+			<h1 class="giveaways-header" style="margin-top: 0px; text-decoration: none;">Current Media</h1>
+		</div>
+		-->
+		<!-- END GIVEAWAY HEADER -->
+		
+		<div class="container">
+
+			<!-- GENERATE GIVEAWAY SLIDE -->
+			<div class="col-md-6 slide-view">
+				<div class="white-box">
+
+					<!--
+					<a href="https://www.facebook.com/Savedin1080p/photos/a.448929068646268.1073741828.447619082110600/451007011771807/?type=3&theater" target="_blank"><img class="giveaway-image" src="css/img/steam.png" style="height: 300px;; width: 100%;" /></a>
+					
+					<div class="under-statement" style="text-align: left;">
+						<h4 style="border-top: 1px solid #efefef; padding: 5px;">$25 Steam Gift Card</h4>
+						<p>Like the page <a href="https://www.facebook.com/Savedin1080p/">Savedin1080p</a> and share <a href="https://www.facebook.com/Savedin1080p/photos/a.448929068646268.1073741828.447619082110600/451007011771807/?type=3&theater">this</a> post to enter for a chance to win. For another entry, follow the instructions in the description of <a href="https://youtu.be/Hq0exNT8Rig">this</a> video.</p>
+					</div>
+					-->
+
+					<a class="twitter-timeline"  href="https://twitter.com/OOIAmSavedOO" data-widget-id="711026158012239872">Tweets by @OOIAmSavedOO</a>
+            				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+				</div>
+			</div>
+			
+			<div class="col-md-6 slide-view">
+				<div class="white-box">
+
+					<!--
+					<a href="http://tinyurl.com/gmkqusv" target="_blank"><img class="giveaway-image" src="css/img/shantae.png" style="height: 300px; width: 100%;" /></a>
+					
+					<div class="under-statement" style="text-align: left;">
+						<h4 style="border-top: 1px solid #efefef; padding: 5px;">Shantae and the Pirate's Curse</h4>
+						<p>Like and share <a href="http://tinyurl.com/gmkqusv" target="_blank">this post</a> to enter for a chance to win a free copy of Shantae and the Pirate's Curse.</p>
+					</div>
+					-->
+
+					<div class="fb-page" style="width: 100%;" data-href="https://www.facebook.com/Savedin1080p/?fref=ts" data-tabs="timeline" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/Savedin1080p/?fref=ts"><a href="https://www.facebook.com/Savedin1080p/?fref=ts">Savedin1080p</a></blockquote></div></div>
+				</div>
+			</div>
+			<!-- END GIVEAWAY SLIDE -->
+
+		</div>
+	</section>
+	<!-- END GIVEAWAY -->
+
+	<!-- BEGIN FOOTER -->
+	<footer class="foot">
+		<div class="container">
+			<div class="row">
+
+				<div class="container">
+
+					<div class="col-md-4">
+						<div class="black-box">
+							<a stlye="color: #fff;" href="http://savedin1080p.com/mybb" target="_blank">
+								<h2>The Forums</h2>
+							</a>
+							<a class="box" href="http://bpa.ictcit.com/mybb" target="_blank"><i class="fa fa-forumbee"></i></a>
+						</div>
+					</div>
+					
+					<div class="col-md-4">
+						<div class="black-box">
+							<a stlye="color: #fff;" href="http://twitter.com/OOIAmSavedOO" target="_blank">
+								<h2>The Twitter</h2>
+							</a>
+							<a class="box" href="http://twitter.com/OOIAmSavedOO" target="_blank"><i class="fa fa-twitter"></i></a>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="black-box">
+							<a stlye="color: #fff;" href="https://www.youtube.com/user/00IAmSaved00" target="_blank">
+								<h2>The Channel</h2>
+							</a>
+							<a class="box" href="https://www.youtube.com/user/00IAmSaved00" target="_blank"><i class="fa fa-youtube"></i></a>
+						</div>
+					</div>
+					
+					<div class="col-md-4">
+						<div class="black-box">
+							<a stlye="color: #fff;" data-scroll href="#home">
+								<h2>Homepage</h2>
+							</a>
+							<a data-scroll class="box" href="#home"><i class="fa fa-home"></i></a>
+						</div>
+					</div>
+					
+					<div class="col-md-4">
+						<div class="black-box">
+							<a stlye="color: #fff;" data-scroll href="#second-half">
+								<h2>Newest Video</h2>
+							</a>
+							<a data-scroll class="box" href="#second-half"><i class="fa fa-play"></i></a>
+						</div>
+					</div>
+
+					<div class="col-md-4">
+						<div class="black-box">
+							<a stlye="color: #fff;" data-scroll href="#giveaway">
+								<h2>Media</h2>
+							</a>
+							<a data-scroll class="box" href="#giveaway"><i class="fa fa-facebook"></i></a>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="col-md-8">
+					<p style="padding: 40px; font-size: 14px;">&copy; Savedin1080p 2016 | Design by <a class="foot" href="http://www.ohhi.net23.net"  target="_blank">oh_hi</a></p>
+				</div>
+
+			</div>
+		</div>
+	</footer>
+	<!-- END FOOTER -->
+
+	<!-- Load Booststrap -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/smooth-scroll.js"></script>
+
+	<!-- Load custom js for theme -->
+	<script type="text/javascript" src="js/app.js"></script>
+	
+	<!-- Load Unslider 
+	<script src="unslider/src/js/unslider.js"></script>
+	<!-- END OMIT -->
+
+</body>
+</html>
